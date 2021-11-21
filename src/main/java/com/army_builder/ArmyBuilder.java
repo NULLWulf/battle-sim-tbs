a23a6ec9cb7d319b237306860372e28a708d5c0b
Nathan <nwolf2@oswego.edu>
2021-11-21 14:30:43 -0500

    sigh

--- src/main/java/com/army_builder/ArmyBuilder.java
+++ src/main/java/com/army_builder/ArmyBuilder.java
@@ -14,24 +14,6 @@ import java.util.ArrayList;
 public class ArmyBuilder {
         public static void main(String[] args) throws IOException {
 
-            battle BattleS = new battle();
-            Ranged range = new Ranged("Array","Nathan",50,50,50,50,50);
-            Infantry infantry = new Infantry("Hi","Hi",50,50,50,50);
-            Cavalry cavalry = new Cavalry("Hi","Hi",50,50,50,50);
-            //            Infantry infantry = new Infantry();
-//            BattleS.unitWeakness(infantry,infantry);
-
-            JSON_Handler json = new JSON_Handler();
-            json.writeObjectAsJson("infantry.json",range);
-
-//            Units[][] unitMap = new Units[4][4];
-//
-//            unitMap[0][0] = infantry;
-//            unitMap[1][1] = range;
-
-            Map map = new Map();
-            map.generateMapBounds();
-            map.displayUnitsOnMap();
 
 
 
