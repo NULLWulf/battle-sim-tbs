b47005e5ad2e15652d278b472d6f2740231a685a
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2021-12-16 21:11:08 -0500

    implemneted grapjt

--- src/main/java/com/army_builder/ArmyBuilder.java
+++ src/main/java/com/army_builder/ArmyBuilder.java
@@ -1,25 +1,18 @@
 package com.army_builder;
 
 
-import com.army_builder.models.*;
-import com.army_builder.systems.ArmyDesigner;
-import com.army_builder.systems.JSON_Handler;
-import com.army_builder.systems.Map;
-import com.army_builder.systems.battle;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
 
 import java.io.IOException;
-import java.nio.file.*;
-import java.util.ArrayList;
 
 public class ArmyBuilder {
         public static void main(String[] args) throws IOException {
-
-            JSON_Handler handler = new JSON_Handler();
-
-            Army army = new Army();
-
-            army.setFaction("Test");
-            handler.writeObjectAsJson(army.getFaction(), army);
+            Graph<Object, DefaultEdge> simpleGraph = new SimpleGraph<>(DefaultEdge.class);
+            simpleGraph.addVertex(50);
+            simpleGraph.addVertex(25);
+            simpleGraph.addVertex(30);
         }
 
     }
---
+++ src/main/java/com/army_builder/systems/Graph.java
@@ -0,0 +1,136 @@
+package com.army_builder.systems;
+
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.function.Supplier;
+
+class DefaultGraph implements Graph {
+
+
+    @Override
+    public Set getAllEdges(Object o, Object v1) {
+        return null;
+    }
+    @Override
+    public Object getEdge(Object o, Object v1) {
+        return null;
+    }
+    @Override
+    public Supplier getVertexSupplier() {
+        return null;
+    }
+    @Override
+    public Supplier getEdgeSupplier() {
+        return null;
+    }
+    @Override
+    public Object addEdge(Object o, Object v1) {
+        return null;
+    }
+    @Override
+    public boolean addEdge(Object o, Object v1, Object o2) {
+        return false;
+    }
+    @Override
+    public Object addVertex() {
+        return null;
+    }
+    @Override
+    public boolean addVertex(Object o) {
+        return false;
+    }
+    @Override
+    public boolean containsEdge(Object o, Object v1) {
+        return false;
+    }
+    @Override
+    public boolean containsEdge(Object o) {
+        return false;
+    }
+    @Override
+    public boolean containsVertex(Object o) {
+        return false;
+    }
+    @Override
+    public Set edgeSet() {
+        return null;
+    }
+    @Override
+    public int degreeOf(Object o) {
+        return 0;
+    }
+    @Override
+    public Set edgesOf(Object o) {
+        return null;
+    }
+    @Override
+    public int inDegreeOf(Object o) {
+        return 0;
+    }
+    @Override
+    public Set incomingEdgesOf(Object o) {
+        return null;
+    }
+    @Override
+    public int outDegreeOf(Object o) {
+        return 0;
+    }
+    @Override
+    public Set outgoingEdgesOf(Object o) {
+        return null;
+    }
+    @Override
+    public boolean removeAllEdges(Collection collection) {
+        return false;
+    }
+    @Override
+    public Set removeAllEdges(Object o, Object v1) {
+        return null;
+    }
+    @Override
+    public boolean removeAllVertices(Collection collection) {
+        return false;
+    }
+    @Override
+    public Object removeEdge(Object o, Object v1) {
+        return null;
+    }
+    @Override
+    public boolean removeEdge(Object o) {
+        return false;
+    }
+    @Override
+    public boolean removeVertex(Object o) {
+        return false;
+    }
+    @Override
+    public Set vertexSet() {
+        return null;
+    }
+    @Override
+    public Object getEdgeSource(Object o) {
+        return null;
+    }
+    @Override
+    public Object getEdgeTarget(Object o) {
+        return null;
+    }
+    @Override
+    public GraphType getType() {
+        return null;
+    }
+    @Override
+    public double getEdgeWeight(Object o) {
+        return 0;
+    }
+    @Override
+    public void setEdgeWeight(Object o, double v) {
+
+    }
+}
